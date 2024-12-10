f0df3f0d46ef7e6f0b5d53caf4a4a63873fe939a
deepakmurugesan07 <imdeepak.murugesan@gmail.com>
2024-12-10 20:02:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const cardbtn=document.querySelector('#card-btn');
+const cart=document.querySelector('.cart')
+const btnClose=document.querySelector('#cart-close');
+btnCart.addEventListener('click')
+
+btnCart.addEventListener('click',() =>{
+    cart.classList.add('cart-active');});
